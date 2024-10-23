eb2761545198395e85fa5c1f285f46b53ab5c6c7
CCranney <11773171+CCranney@users.noreply.github.com>
2024-10-23 08:56:57 -0700

    fixed bugs, added more tests

--- src/attention_smithy/attention/StandardAttentionMethod.py
+++ src/attention_smithy/attention/StandardAttentionMethod.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from attention_smithy.utils import create_causal_mask
 
 
 class StandardAttentionMethod(nn.Module):
@@ -10,21 +11,24 @@ class StandardAttentionMethod(nn.Module):
         through this module.
     """
 
-    def __init__(self, dropout: float = 0.0):
+    def __init__(self, dropout: float = 0.0, is_causal_masking: bool = False):
         """
         Initializes the standard attention module.
 
         Args:
             dropout (float, optional): The dropout probability. Defaults to 0.0.
+            is_causal_masking (bool, optional): When set to true, tokens can only be attended to by past tokens.
+                This is most often seen decoder models like GPT. Defaults to false.
         """
 
         super().__init__()
         self.dropout_layer = nn.Dropout(dropout)
+        self.is_causal_masking = is_causal_masking
 
     def forward(
         self,
-        k,
         q,
+        k,
         v,
         **kwargs,
     ):
@@ -39,8 +43,26 @@ class StandardAttentionMethod(nn.Module):
         Returns:
             torch.Tensor: The output tensor, of shape (batch_size, num_heads, query_length, head_dim)
         """
-        attn_scores = torch.einsum("bnqd, bnkd -> bnqk", q, k)
+        attn_scores = self._calculate_query_by_key_attention_scores(q, k)
+        #attn_scores += positional_embedding.calculate_alibi_positional_embedding(q, k)
+        attn_scores = self._apply_diagonal_causal_masking_if_relevant(attn_scores)
         attn_probs = F.softmax(attn_scores, dim=-1)
         attn_probs = self.dropout_layer(attn_probs)
-        attn_output = torch.einsum("bnqk, bnkd -> bnqd", attn_probs, v)
+        attn_output = torch.matmul(attn_probs, v)
         return attn_output, attn_probs
+
+    def _calculate_query_by_key_attention_scores(self, q, k):
+        head_dimension = q.shape[-1]
+        attn_scores = torch.matmul(q, k.transpose(-2, -1))
+        attn_scores /= torch.sqrt(
+            torch.tensor(head_dimension, dtype=torch.float32)
+        )
+        return attn_scores
+
+    def _apply_diagonal_causal_masking_if_relevant(self, attn_scores):
+        if self.is_causal_masking:
+            causal_mask = create_causal_mask(attn_scores.size(-1))
+            attn_scores = attn_scores.masked_fill(causal_mask == 0, float("-inf"))
+        return attn_scores
+
+
--- tests/unit/attention/test_StandardAttentionMethod.py
+++ tests/unit/attention/test_StandardAttentionMethod.py
@@ -1,21 +1,260 @@
 import torch
 from attention_smithy.attention import StandardAttentionMethod
 
-def test__StandardAttentionMethod__forward_pass_functions_as_expected():
-    q = torch.tensor([[[[1e-1, 2e-1, 3e-1], [4e-1, 5e-1, 6e-1]]]])
-    k = torch.tensor([[[[1e-1, 2e-1, 3e-1], [4e-1, 5e-1, 6e-1]]]])
-    v = torch.tensor([[[[1e-1, 2e-1, 3e-1], [4e-1, 5e-1, 6e-1]]]])
+def test__StandardAttentionMethod__forward_pass_functions_as_expected__dimension3_queryLength2_kvLength2():
+    q = torch.tensor([[[
+        [1e-1, 2e-1, 3e-1],
+        [4e-1, 5e-1, 6e-1]
+    ]]])
+    k = torch.tensor([[[
+        [1e-1, 2e-1, 3e-1],
+        [4e-1, 5e-1, 6e-1]
+    ]]])
+    v = torch.tensor([[[
+        [1e-1, 2e-1, 3e-1],
+        [4e-1, 5e-1, 6e-1]
+    ]]])
     attention = StandardAttentionMethod()
     output, attn_probs = attention(q, k, v)
 
     expected_attn_probs = torch.tensor([[[
-        [0.4551, 0.5449],
-        [0.3894, 0.6106]
+        [0.4740, 0.5260],
+        [0.4354, 0.5646]
     ]]])
     expected_output = torch.tensor([[[
-        [0.2635, 0.3635, 0.4635],
-        [0.2832, 0.3832, 0.4832]
+        [0.2578, 0.3578, 0.4578],
+        [0.2694, 0.3694, 0.4694]
     ]]])
+
+    assert torch.allclose(attn_probs, expected_attn_probs, atol=1e-4)
+    assert torch.allclose(output, expected_output, atol=1e-4)
+
+def test__StandardAttentionMethod__forward_pass_functions_as_expected__dimension3_queryLength2_kvLength4():
+    q = torch.tensor([[[[1e-1, 2e-1, 3e-1],
+                        [4e-1, 5e-1, 6e-1]]]])
+    k = torch.tensor([[[[1e-1, 2e-1, 3e-1],
+                        [4e-1, 5e-1, 6e-1],
+                        [1e-1, 2e-1, 3e-1],
+                        [4e-1, 5e-1, 6e-1]]]])
+    v = torch.tensor([[[[1e-1, 2e-1, 3e-1],
+                        [4e-1, 5e-1, 6e-1],
+                        [1e-1, 2e-1, 3e-1],
+                        [4e-1, 5e-1, 6e-1]]]])
+    attention = StandardAttentionMethod()
+
+    output, attn_probs = attention(q, k, v)
+
+    expected_attn_probs = torch.tensor([[[
+        [0.2370, 0.2630, 0.2370, 0.2630],
+        [0.2177, 0.2823, 0.2177, 0.2823]
+    ]]])
+    expected_output = torch.tensor([[[
+        [0.2578, 0.3578, 0.4578],
+        [0.2694, 0.3694, 0.4694]
+    ]]])
+
+    assert torch.allclose(attn_probs, expected_attn_probs, atol=1e-4)
+    assert torch.allclose(output, expected_output, atol=1e-4)
+
+def test__StandardAttentionMethod__forward_pass_functions_as_expected__dimension3_queryLength2_kvLength2__apply_causal_masking():
+    q = torch.tensor([[[
+        [1e-1, 2e-1, 3e-1],
+        [4e-1, 5e-1, 6e-1]
+    ]]])
+    k = torch.tensor([[[
+        [1e-1, 2e-1, 3e-1],
+        [4e-1, 5e-1, 6e-1]
+    ]]])
+    v = torch.tensor([[[
+        [1e-1, 2e-1, 3e-1],
+        [4e-1, 5e-1, 6e-1]
+    ]]])
+    attention = StandardAttentionMethod(is_causal_masking=True)
+    output, attn_probs = attention(q, k, v)
+
+    expected_attn_probs = torch.tensor([[[
+        [1.0, 0],
+        [0.4354, 0.5646]
+    ]]])
+    expected_output = torch.tensor([[[
+        [1e-1, 2e-1, 3e-1],
+        [0.2694, 0.3694, 0.4694]
+    ]]])
+
+    assert output.shape == q.shape
+    assert torch.allclose(attn_probs, expected_attn_probs, atol=1e-4)
+    assert torch.allclose(output, expected_output, atol=1e-4)
+
+def test__StandardAttentionMethod__forward_pass_functions_as_expected__dimension3_queryLength2_kvLength2__batchSize2():
+    q = torch.tensor([
+        [[
+            [1e-1, 2e-1, 3e-1],
+            [4e-1, 5e-1, 6e-1]
+        ]],
+        [[
+            [15e-2, 25e-2, 35e-2],
+            [45e-2, 55e-2, 65e-2]
+        ]]
+    ])
+    k = torch.tensor([
+        [[
+            [1e-1, 2e-1, 3e-1],
+            [4e-1, 5e-1, 6e-1]
+        ]],
+        [[
+            [15e-2, 25e-2, 35e-2],
+            [45e-2, 55e-2, 65e-2]
+        ]]
+    ])
+    v = torch.tensor([
+        [[
+            [1e-1, 2e-1, 3e-1],
+            [4e-1, 5e-1, 6e-1]
+        ]],
+        [[
+            [15e-2, 25e-2, 35e-2],
+            [45e-2, 55e-2, 65e-2]
+        ]]
+    ])
+    attention = StandardAttentionMethod()
+    output, attn_probs = attention(q, k, v)
+
+    expected_attn_probs = torch.tensor([
+        [[
+            [0.4740, 0.5260],
+            [0.4354, 0.5646]
+        ]],
+        [[
+            [0.4676, 0.5324],
+            [0.4290, 0.5710]
+        ]],
+    ])
+    expected_output = torch.tensor([
+        [[
+            [0.2578, 0.3578, 0.4578],
+            [0.2694, 0.3694, 0.4694]
+        ]],
+        [[
+            [0.3097, 0.4097, 0.5097],
+            [0.3213, 0.4213, 0.5213]
+        ]],
+    ])
+
+    assert torch.allclose(attn_probs, expected_attn_probs, atol=1e-4)
+    assert torch.allclose(output, expected_output, atol=1e-4)
+
+def test__StandardAttentionMethod__forward_pass_functions_as_expected__dimension3_queryLength2_kvLength2__numHeads2():
+    q = torch.tensor([[
+        [
+            [1e-1, 2e-1, 3e-1],
+            [4e-1, 5e-1, 6e-1]
+        ],
+        [
+            [15e-2, 25e-2, 35e-2],
+            [45e-2, 55e-2, 65e-2]
+        ]
+    ]])
+    k = torch.tensor([[
+        [
+            [1e-1, 2e-1, 3e-1],
+            [4e-1, 5e-1, 6e-1]
+        ],
+        [
+            [15e-2, 25e-2, 35e-2],
+            [45e-2, 55e-2, 65e-2]
+        ]
+    ]])
+    v = torch.tensor([[
+        [
+            [1e-1, 2e-1, 3e-1],
+            [4e-1, 5e-1, 6e-1]
+        ],
+        [
+            [15e-2, 25e-2, 35e-2],
+            [45e-2, 55e-2, 65e-2]
+        ]
+    ]])
+    attention = StandardAttentionMethod()
+    output, attn_probs = attention(q, k, v)
+
+    expected_attn_probs = torch.tensor([[
+        [
+            [0.4740, 0.5260],
+            [0.4354, 0.5646]
+        ],
+        [
+            [0.4676, 0.5324],
+            [0.4290, 0.5710]
+        ],
+    ]])
+    expected_output = torch.tensor([[
+        [
+            [0.2578, 0.3578, 0.4578],
+            [0.2694, 0.3694, 0.4694]
+        ],
+        [
+            [0.3097, 0.4097, 0.5097],
+            [0.3213, 0.4213, 0.5213]
+        ],
+    ]])
+
+    assert torch.allclose(attn_probs, expected_attn_probs, atol=1e-4)
+    assert torch.allclose(output, expected_output, atol=1e-4)
+
+def test__StandardAttentionMethod__forward_pass_functions_as_expected__dimension3_queryLength2_kvLength2__numHeads2__apply_causal_masking():
+    q = torch.tensor([[
+        [
+            [1e-1, 2e-1, 3e-1],
+            [4e-1, 5e-1, 6e-1]
+        ],
+        [
+            [15e-2, 25e-2, 35e-2],
+            [45e-2, 55e-2, 65e-2]
+        ]
+    ]])
+    k = torch.tensor([[
+        [
+            [1e-1, 2e-1, 3e-1],
+            [4e-1, 5e-1, 6e-1]
+        ],
+        [
+            [15e-2, 25e-2, 35e-2],
+            [45e-2, 55e-2, 65e-2]
+        ]
+    ]])
+    v = torch.tensor([[
+        [
+            [1e-1, 2e-1, 3e-1],
+            [4e-1, 5e-1, 6e-1]
+        ],
+        [
+            [15e-2, 25e-2, 35e-2],
+            [45e-2, 55e-2, 65e-2]
+        ]
+    ]])
+    attention = StandardAttentionMethod(is_causal_masking=True)
+    output, attn_probs = attention(q, k, v)
+
+    expected_attn_probs = torch.tensor([[
+        [
+            [1.0, 0.0],
+            [0.4354, 0.5646]
+        ],
+        [
+            [1.0, 0.0],
+            [0.4290, 0.5710]
+        ],
+    ]])
+    expected_output = torch.tensor([[
+        [
+            [1e-1, 2e-1, 3e-1],
+            [0.2694, 0.3694, 0.4694]
+        ],
+        [
+            [15e-2, 25e-2, 35e-2],
+            [0.3213, 0.4213, 0.5213]
+        ],
+    ]])
 
     assert torch.allclose(attn_probs, expected_attn_probs, atol=1e-4)
     assert torch.allclose(output, expected_output, atol=1e-4)
