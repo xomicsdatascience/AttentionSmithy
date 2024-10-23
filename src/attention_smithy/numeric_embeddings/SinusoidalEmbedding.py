aa52587410f129a06c92d80d24c842025d41838b
CCranney <11773171+CCranney@users.noreply.github.com>
2024-10-23 11:56:35 -0700

    updated sinusoidal embedding to be able to encode given, customized values

--- src/attention_smithy/numeric_embeddings/SinusoidalEmbedding.py
+++ src/attention_smithy/numeric_embeddings/SinusoidalEmbedding.py
@@ -1,15 +1,36 @@
 import torch
 
+
 class SinusoidalEmbedding:
     def __init__(self, d_model, max_len=5000):
+        self.exponent = torch.arange(0, d_model, 2) / d_model
+        self._initialize_all_possible_positional_encodings(d_model, max_len)
+
+    def __call__(self, x, custom_values=None):
+        if custom_values == None:
+            _, sequence_length, _ = x.shape
+            return self.positional_encoding[:sequence_length, :].to(x.device)
+        else:
+            sinusoidal_embedding = self._encode_float_values(x, custom_values)
+            return sinusoidal_embedding
+
+    def _initialize_all_possible_positional_encodings(self, d_model, max_len):
         self.positional_encoding = torch.zeros(max_len, d_model)
         self.positional_encoding.requires_grad = False
+        positions = torch.arange(0, max_len).unsqueeze(1)
+        sin, cos = self._find_sin_and_cos_embeddings_of_given_values(positions)
+        self.positional_encoding[:, 0::2] = sin
+        self.positional_encoding[:, 1::2] = cos
 
-        pos = torch.arange(0, max_len).unsqueeze(1)
-        _2i = torch.arange(0, d_model, 2)
-        self.positional_encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
-        self.positional_encoding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / d_model)))
+    def _encode_float_values(self, x, custom_values):
+        sinusoidal_embedding = torch.zeros_like(x)
+        sin, cos = self._find_sin_and_cos_embeddings_of_given_values(
+            custom_values.unsqueeze(2)
+        )
+        sinusoidal_embedding[:, :, 0::2] = sin
+        sinusoidal_embedding[:, :, 1::2] = cos
+        return sinusoidal_embedding
 
-    def __call__(self, x):
-        batch_size, seq_len, d_model = x.size()
-        return self.encoding[:seq_len, :].to(x.device)
+    def _find_sin_and_cos_embeddings_of_given_values(self, initial_values):
+        values = initial_values / (10_000**self.exponent)
+        return torch.sin(values), torch.cos(values)
--- tests/unit/numeric_embeddings/test_SinusoidalEmbedding.py
+++ tests/unit/numeric_embeddings/test_SinusoidalEmbedding.py
@@ -1,17 +1,137 @@
 import torch
 from attention_smithy.numeric_embeddings import SinusoidalEmbedding
 
+
 def test__SinusoidalEmbedding__positional_encoding_has_expected_shape():
     embedding_dimension_size = 128
     max_len = 100
     embedding = SinusoidalEmbedding(d_model=embedding_dimension_size, max_len=max_len)
     assert embedding.positional_encoding.shape == (max_len, embedding_dimension_size)
 
+
 def test__SinusoidalEmbedding__position_values_encode_correctly():
-    embedding = SinusoidalEmbedding(d_model=10, max_len=2)
-    expected_encoding = torch.tensor([
-        [0.0000e+00, 1.0000e+00, 0.0000e+00, 1.0000e+00, 0.0000e+00, 1.0000e+00,
-         0.0000e+00, 1.0000e+00, 0.0000e+00, 1.0000e+00],
-        [8.4147e-01, 5.4030e-01, 1.5783e-01, 9.8747e-01, 2.5116e-02, 9.9968e-01,
-         3.9811e-03, 9.9999e-01, 6.3096e-04, 1.0000e+00]])
+    embedding = SinusoidalEmbedding(d_model=10, max_len=4)
+
+    expected_encoding = torch.tensor(
+        [
+            [
+                0.0000e00,
+                1.0000e00,
+                0.0000e00,
+                1.0000e00,
+                0.0000e00,
+                1.0000e00,
+                0.0000e00,
+                1.0000e00,
+                0.0000e00,
+                1.0000e00,
+            ],
+            [
+                8.4147e-01,
+                5.4030e-01,
+                1.5783e-01,
+                9.8747e-01,
+                2.5116e-02,
+                9.9968e-01,
+                3.9811e-03,
+                9.9999e-01,
+                6.3096e-04,
+                1.0000e00,
+            ],
+            [
+                9.0930e-01,
+                -4.1615e-01,
+                3.1170e-01,
+                9.5018e-01,
+                5.0217e-02,
+                9.9874e-01,
+                7.9621e-03,
+                9.9997e-01,
+                1.2619e-03,
+                1.0000e00,
+            ],
+            [
+                1.4112e-01,
+                -9.8999e-01,
+                4.5775e-01,
+                8.8908e-01,
+                7.5285e-02,
+                9.9716e-01,
+                1.1943e-02,
+                9.9993e-01,
+                1.8929e-03,
+                1.0000e00,
+            ],
+        ]
+    )
     assert torch.allclose(embedding.positional_encoding, expected_encoding, atol=1e-4)
+
+
+def test__SinusoidalEmbedding__float_values_encode_correctly():
+    embedding = SinusoidalEmbedding(d_model=10, max_len=2)
+    x = torch.rand(2, 2, 10)
+    custom_values = torch.tensor(
+        [
+            [0.0, 1.0],
+            [2.0, 3.0],
+        ]
+    )
+
+    encoding = embedding(x=x, custom_values=custom_values)
+    expected_encoding = torch.tensor(
+        [
+            [
+                [
+                    0.0000e00,
+                    1.0000e00,
+                    0.0000e00,
+                    1.0000e00,
+                    0.0000e00,
+                    1.0000e00,
+                    0.0000e00,
+                    1.0000e00,
+                    0.0000e00,
+                    1.0000e00,
+                ],
+                [
+                    8.4147e-01,
+                    5.4030e-01,
+                    1.5783e-01,
+                    9.8747e-01,
+                    2.5116e-02,
+                    9.9968e-01,
+                    3.9811e-03,
+                    9.9999e-01,
+                    6.3096e-04,
+                    1.0000e00,
+                ],
+            ],
+            [
+                [
+                    9.0930e-01,
+                    -4.1615e-01,
+                    3.1170e-01,
+                    9.5018e-01,
+                    5.0217e-02,
+                    9.9874e-01,
+                    7.9621e-03,
+                    9.9997e-01,
+                    1.2619e-03,
+                    1.0000e00,
+                ],
+                [
+                    1.4112e-01,
+                    -9.8999e-01,
+                    4.5775e-01,
+                    8.8908e-01,
+                    7.5285e-02,
+                    9.9716e-01,
+                    1.1943e-02,
+                    9.9993e-01,
+                    1.8929e-03,
+                    1.0000e00,
+                ],
+            ],
+        ]
+    )
+    assert torch.allclose(encoding, expected_encoding, atol=1e-4)
