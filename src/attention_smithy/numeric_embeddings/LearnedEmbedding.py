afd1614a4a6c731fa4da91ce729e30aabc984a74
CCranney <11773171+CCranney@users.noreply.github.com>
2024-10-24 06:13:08 -0700

    adding learned embedding

---
+++ src/attention_smithy/numeric_embeddings/LearnedEmbedding.py
@@ -0,0 +1,28 @@
+from torch import nn
+
+class LearnedEmbedding(nn.Module):
+    """
+    A pytorch module for learned positional embeddings.
+    """
+    def __init__(self, vocab_size, embedding_dimension, padding_idx=0):
+        """
+        Args:
+            vocab_size (int): The number of values in the expected vocabulary.
+            embedding_dimension (int): The embedding dimension size.
+            padding_idx (int): The index assigned to ignorable padding tokens.
+        Attributes:
+            embedding (nn.Embedding): a module to pass position or other sequences
+                to for embedding.
+        """
+        self.embedding = nn.Embedding(vocab_size, embedding_dimension, padding_idx=padding_idx)
+
+    def forward(self, x):
+        """
+        Args:
+            x (torch.Tensor): An input tensor of values to be encoded, of shape
+                (batch_size, sequence_length).
+        Returns:
+            torch.Tensor: An output tensor of shape
+                (batch_size, sequence_length, embedding_dimension)
+        """
+        return embedding(x)
